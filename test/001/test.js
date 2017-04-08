f6fbe203d529464a699780d0afbf1b6290ffb725
Kei Ito <kei.itof@gmail.com>
2017-04-08 21:05:52 +0900

    remove async/await from test

--- test/001/test.js
+++ test/001/test.js
@@ -3,17 +3,19 @@ const assert = require('assert');
 const {rollup} = require('rollup');
 const globImport = require('../..');
 
-it('should parse *', async function () {
-	const bundle = await rollup({
+it('should parse *', function () {
+	return rollup({
 		entry: path.join(__dirname, 'src.js'),
 		plugins: [
 			globImport({debug: true})
 		]
+	})
+	.then((bundle) => {
+		const {code} = bundle.generate({format: 'es'});
+		assert.deepEqual(code.trim().split(/\s+/), [
+			'console.log(\'001\');',
+			'console.log(\'002\');',
+			'console.log(\'003\');'
+		]);
 	});
-	const {code} = bundle.generate({format: 'es'});
-	assert.deepEqual(code.trim().split(/\s+/), [
-		'console.log(\'001\');',
-		'console.log(\'002\');',
-		'console.log(\'003\');'
-	]);
 });
