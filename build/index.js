62d6dd03efa03cd48fd07b6fc4ab3bdc55eadde0
Kei Ito <kei.itof@gmail.com>
2017-09-15 19:03:31 +0900

    build before run

--- build/index.js
+++ build/index.js
@@ -1,42 +1,35 @@
 const path = require('path');
 const fs = require('fs');
-const $console = require('j1/console').create('build');
-const promisify = require('j1/promisify');
-const writeFile = require('j1/writeFile');
-const cp = require('j1/cp');
-const glob = promisify(require('glob'));
-const readFile = promisify(fs.readFile, fs);
+const glob = require('glob');
 const babel = require('babel-core');
+const childProcess = require('child_process');
 
-async function build() {
+function build() {
 	const projectRoot = path.join(__dirname, '..');
 	const srcDirectory = path.join(projectRoot, 'src');
-	const files = await glob(path.join(srcDirectory, '**', '*.js'));
-	for (const file of files) {
-		const console = $console.create(path.relative(projectRoot, file));
+	glob.sync(path.join(srcDirectory, '**', '*'), {nodir: true})
+	.forEach((file) => {
+		console.log(path.relative(projectRoot, file));
 		const dest = path.join(projectRoot, 'dist', path.relative(srcDirectory, file));
-		if ((/test.*src/).test(file)) {
-			await cp(file, dest);
+		childProcess.execSync(`mkdir -p ${path.dirname(dest)}`);
+		const input = fs.readFileSync(file);
+		if (path.extname(file) === '.js') {
+			const code = babel.transform([
+				'import "babel-polyfill";',
+				input.toString('utf8')
+			].join('\n'), {presets: [['env', {
+				targets: {node: '4'},
+				useBuiltIns: true
+			}]]}).code;
+			fs.writeFileSync(dest, code);
 		} else {
-			console.info('read');
-			const input = await readFile(file, 'utf8');
-			console.info('transpile');
-			const code = [
-				'require(\'regenerator-runtime/runtime\');',
-				babel.transform(input, {presets: [['env', {targets: {node: '4'}}]]}).code
-			].join('\n');
-			console.info('write');
-			await writeFile(dest, code);
+			fs.writeFileSync(dest, input);
 		}
-	}
+	});
 }
 
 module.exports = build;
 
 if (!module.parent) {
-	build()
-	.catch((error) => {
-		$console.error(error);
-		process.exit(1);
-	});
+	build();
 }
