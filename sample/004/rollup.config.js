08a0c42135a287a73303bd8dd375493a0b8d9140
Kei Ito <kei.itof@gmail.com>
2018-04-23 22:54:05 +0900

    fix indent

--- sample/004/rollup.config.js
+++ sample/004/rollup.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 const globImport = require('../..');
 module.exports = {
 	input: 'input.js',
-	plugins: [globImport({
-		rename(name, id) {
-			return `${path.relative(__dirname, id)}/${name}`.replace(/[^\w]/g, '_');
-		},
-	})],
+	plugins: [
+		globImport({
+			rename(name, id) {
+				return `${path.relative(__dirname, id)}/${name}`.replace(/[^\w]/g, '_');
+			},
+		}),
+	],
 	output: {
 		file: 'output.js',
 		format: 'es',
